867e169498c616762ac559c9741ef752f0e03a73
s11142 <shotanagaizumi@gmail.com>
2024-05-12 16:22:16 +0900

    feat: FizzBuzzを実装

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,7 +1,23 @@
 public class FizzBuzz {
 
+    public String convert(int num) {
+        if (isMultipleOfThree(num) && isMultipleOfFive(num)) {
+            return "FizzBuzz";
+        }
+        if (isMultipleOfThree(num)) {
+            return "Fizz";
+        }
+        if (isMultipleOfFive(num)) {
+            return "Buzz";
+        }
+        return String.valueOf(num);
+    }
 
-    public String convert(int i) {
-        return "1";
+    private boolean isMultipleOfFive(int num) {
+        return num % 5 == 0;
+    }
+
+    private boolean isMultipleOfThree(int num) {
+        return num % 3 == 0;
     }
 }
--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -1,16 +1,55 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
+@DisplayName("Fizz Buzz 数列と変換規則を扱う FizzBuzzクラス")
 class FizzBuzzTest {
 
-    @Test
-    void _1を渡すと文字列1を返す() {
-        // 準備(Arrange)
-        FizzBuzz fizzBuzz = new FizzBuzz();
-        // 実行(Act)
-        String actual = fizzBuzz.convert(1);
-        // 検証(Assert)
-        assertEquals("1", actual);
+    private FizzBuzz fizzBuzz;
+
+    @BeforeEach
+    void 前準備() {
+        fizzBuzz = new FizzBuzz();
     }
+
+    @Nested
+    class convertメソッドは数を文字列に変換する {
+        @Nested
+        class その他の数のときはそのまま文字列に変換する {
+            @Test
+            void _1を渡すと文字列1を返す() {
+                // 実行(Act) && 検証(Assert)
+                assertEquals("1", fizzBuzz.convert(1));
+            }
+        }
+
+        @Nested
+        class _3の倍数の場合は数字の代わりにFizzに変換する {
+            @Test
+            void _3を渡すと文字列Fizzを返す() {
+                assertEquals("Fizz", fizzBuzz.convert(3));
+            }
+        }
+
+        @Nested
+        class _5の倍数の場合は数字の代わりにBuzzに変換する {
+            @Test
+            void _5を渡すと文字列Buzzを返す() {
+                assertEquals("Buzz", fizzBuzz.convert(5));
+            }
+        }
+
+        @Nested
+        class _3と5の倍数の場合は数字の代わりにFizzBuzzに変換する {
+            @Test
+            void _15を渡すと文字列FizzBuzzを返す() {
+                assertEquals("FizzBuzz", fizzBuzz.convert(15));
+            }
+        }
+    }
+
+
 }
